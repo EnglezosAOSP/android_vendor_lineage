55304b94f6c202e1068e4a1c43622b79948628e4
penglezos <panagiotisegl@gmail.com>
2023-06-26 20:03:11 +0300

    Revert "config: Do not build Updater"
    This reverts commit 319862387a1eec62107f731b248f8f42786c715f.

--- config/common.mk
+++ config/common.mk
@@ -113,7 +113,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     LineageParts \
     LineageSettingsProvider \
-    LineageSetupWizard
+    LineageSetupWizard \
+    Updater
 
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/etc/init/init.lineage-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.lineage-updater.rc
